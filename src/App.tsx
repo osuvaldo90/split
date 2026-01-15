04bff5cd97201e29363ff978fc319f0e75fc3967
Osuvaldo Ramos <osuvaldo.ramos@gmail.com>
2026-01-15 01:33:26 -0500

    fix(10.1-02): remove Join page and /join route
    - Delete src/pages/Join.tsx (all functionality now in Home.tsx)
    - Remove /join route from App.tsx
    - Join bill flow now available directly from home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Session from "./pages/Session";
-import Join from "./pages/Join";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="bill/:code" element={<Session />} />
-          <Route path="join" element={<Join />} />
         </Route>
       </Routes>
     </BrowserRouter>
