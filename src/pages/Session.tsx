3f83cd87c07b5dc30413046244703b81b48b2540
eggplantzzz <tmfeickert@gmail.com>
2026-05-23 12:39:03 -0400

    run prettier

--- src/pages/Session.tsx
+++ src/pages/Session.tsx
@@ -571,10 +571,7 @@ export default function Session() {
                   Items {items && items.length > 0 ? `(${items.length})` : ""}
                 </h2>
                 <div className="text-sm text-gray-400 pt-1">
-                  {session.merchant ?
-                    session.merchant :
-                    null
-                  }
+                  {session.merchant ? session.merchant : null}
                 </div>
               </div>
               <div className="space-y-1">
