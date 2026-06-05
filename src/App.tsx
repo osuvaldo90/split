097cffeabe602b92ab720bd950f08d53fb128953
eggplantzzz <tmfeickert@gmail.com>
2026-06-05 12:43:04 -0400

    redirect /bill/:id to /bill/:id/items

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import Layout from "./components/Layout";
 import Items from "./components/Items";
 import TaxTipSettings from "./components/TaxTipSettings";
@@ -13,6 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="bill/:code" element={<Session />}>
+            <Route index element={<Navigate to={"items"} replace />} />
             <Route path="items" element={<Items />} />
             <Route path="taxtip" element={<TaxTipSettings />} />
             <Route path="summary" element={<Summary />} />
